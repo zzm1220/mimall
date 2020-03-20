4a5171c0202d6011ac06a28fe67c7d9ad53c7502
zhimin <1026938098@qq.com>
2020-03-20 08:48:05 +0800

    completed swipter of index page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,32 +1,103 @@
 <!-- 组件说明 -->
 <template>
-    <div class="index">
-        index page
-        <ServiceBar />
+  <div class="index">
+    <div class="container">
+      <div class="swiper-box">
+        <swiper :options="swiperOption" ref="mySwiper">
+          <!-- slides -->
+          <swiper-slide v-for="slide in slideList" :key="slide.img">
+            <a :href="`/product/${slide.id}`" target="_blank">
+              <img :src="slide.img" />
+            </a>
+          </swiper-slide>
+          <!-- Optional controls -->
+          <div class="swiper-pagination" slot="pagination"></div>
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+      </div>
+      <div class="ads-box"></div>
+      <div class="banner"></div>
+      <div class="product-box"></div>
     </div>
+    <ServiceBar />
+  </div>
 </template>
 
 <script>
-    import ServiceBar from '../components/ServiceBar'
-    export default {
-        name: "index",
-        components: {
-           ServiceBar
-        },
-        data () {
-            return {
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+import "swiper/dist/css/swiper.css";
 
-            };
+import ServiceBar from "../components/ServiceBar";
+export default {
+  name: "index",
+  components: {
+    swiper,
+    swiperSlide,
+    ServiceBar
+  },
+  data() {
+    return {
+      swiperOption: {
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
         },
-        computed: {
-
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
         },
-        methods: {
-
+        loop: true,
+        effect: "cube",
+        autoplay: true
+        // some swiper options/callbacks
+        // 所有的参数同 swiper 官方 api 参数
+        // ...
+      },
+      slideList: [
+        {
+          id: "42",
+          img: "/imgs/slider/slide-1.jpg"
         },
+        {
+          id: "45",
+          img: "/imgs/slider/slide-2.jpg"
+        },
+        {
+          id: "46",
+          img: "/imgs/slider/slide-3.jpg"
+        },
+        {
+          id: "",
+          img: "/imgs/slider/slide-4.jpg"
+        },
+        {
+          id: "",
+          img: "/imgs/slider/slide-5.jpg"
+        }
+      ]
+    };
+  },
+  computed: {
+    swiper() {
+      return this.$refs.mySwiper.swiper;
     }
+  },
+  methods: {}
+};
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+@import "../assets/scss/base.scss";
+@import "../assets/scss/mixin.scss";
+.index {
+  .swiper-box {
+    .swiper-container {
+        height: 451px;
+        img {
+            width: 100%;
+        }
+    }
+  }
+}
 </style>
